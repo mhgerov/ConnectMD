4f5f7ee6fa1f64e245a5bee250f57dd8ade71239
Abel Georges <abelgr8@gmail.comgit config --global user.name Abel>
2018-04-30 13:54:49 -0400

    HandleBars structure setup with no design

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var path = require('path');
 
 router.get('/', function (req, res) {
-	res.send('<h1>It works!</h1>');
+	// res.send('<h1>It works!</h1>');
+	res.render("index");
 });
 
 module.exports = router;
